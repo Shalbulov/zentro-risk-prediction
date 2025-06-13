b76bb2a9ef81db26d08a410a18f0a3bf66f39660
Arsen Anarbek <shalbulovarsen0@gmail.com>
2025-06-13 14:21:59 +0500

    «A5»

--- db.js
+++ db.js
@@ -4,13 +4,12 @@ dotenv.config();
 import pkg from "pg";
 const { Pool } = pkg;
 
+// Используем только connectionString и ssl!
 const pool = new Pool({
-  user: process.env.PGUSER,         // например: postgres
-  host: process.env.PGHOST,         // localhost
-  database: process.env.PGDATABASE, // my_auth_db
-  password: process.env.PGPASSWORD, // твой пароль
-  port: Number(process.env.PGPORT), // 5432
-  ssl: false, // ❌ отключаем SSL (важно!)
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Render требует SSL!
+  },
 });
 
 export default pool;
